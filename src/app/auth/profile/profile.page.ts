ed4e89c33363493d57eadcd4f7d9560425bf2a17
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2024-05-31 23:29:26 -0400

    building the profile page

--- src/app/auth/profile/profile.page.ts
+++ src/app/auth/profile/profile.page.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, inject, AfterViewInit } from '@angular/core';
+import { AuthService } from '../auth-service.service';
+import { NotificationService } from 'src/app/notification.service';
+import { Router } from '@angular/router';
+import { Subscription, Observable, from } from 'rxjs';
+import { Firestore, collection, addDoc, setDoc, collectionData, collectionGroup, DocumentReference } from '@angular/fire/firestore';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  authService = inject(AuthService)
+  notifications = inject(NotificationService)
+  router = inject(Router)
+  firestore = inject(Firestore)
+  
   constructor() { }
 
   ngOnInit() {
